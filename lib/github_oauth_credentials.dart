dbfeca0d24ab611a7168ac28ce65e1a09ebba21f
Yara Astro <93931829+YaraAstro@users.noreply.github.com>
2024-11-04 21:26:03 +0530

    finish colab

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23liMlr9coxno8WLqj';
+const githubClientSecret = 'a31cb9b1a16017d7ef9e64335c5dc14f0f36292d';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
